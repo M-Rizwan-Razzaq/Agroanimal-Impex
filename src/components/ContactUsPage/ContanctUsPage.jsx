b15df80e22851cfa3e5114c31501a16df7a2ea1b
Dipanshu-verma <vermadipanshu444@gmail.com>
2023-11-27 18:47:13 +0530

    common

--- src/components/ContactUsPage/ContanctUsPage.jsx
+++ src/components/ContactUsPage/ContanctUsPage.jsx
@@ -6,68 +6,24 @@ import { MdOutlineMailOutline } from "react-icons/md";
 import { CalendarDaysIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
 import secImg from "../../images/TriasseaIcon.png";
 import "./ContactUsPage.css";
+import CompoHeader from "../common/CompoHeader";
 
 const ContactUsPage = () => {
-  const containerStyle = {
-    position: "relative",
-    textAlign: "center",
-    color: "rgb(201, 236, 236)",
-  };
+ 
 
-  const imageStyle = {
-    width: "100%",
-  };
+  
 
-  const textOnImageStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    fontSize: "70px",
-    // fontStyle: "bold",
-    lineHeight: "0.02rem",
-    marginTop: "0.8rem",
-  };
-
-  const links = {
-    position: "absolute",
-    top: "37%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    fontSize: "23px",
-    fontFamily: "cursive",
-    display: "flex",
-    justifyContent: "center",
-    width: "100%", // Ensure the links take up the full width
-  };
-
-  const linkStyle = {
-    margin: "0 15px",
-    transition: "all 0.2s ease-in-out",
-    // marginTop: "9%", // Adjust the margin as needed
-  };
-
+  
   const handleSubmit = (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
     // Add your form submission logic here
   };
   return (
     <div>
-      <div className="something mt-[4.45%]" style={containerStyle}>
-        <img
-          className="contactimage h-[400px]"
-          src={contactUsImage}
-          alt="Contact Us"
-          style={imageStyle}
-        />
-        <div className="flex justify-between items-center " style={links}>
-          <a className="hover:text-yellow-400" style={linkStyle} href="/">
-            Home
-          </a>
-          <h1 style={linkStyle}>Contact</h1>
-        </div>
-        <h1 style={textOnImageStyle}>Contact</h1>
-      </div>
+
+       <CompoHeader name="contact"  image={contactUsImage}/>
+
+
       <div className="main-box grid grid-cols-3 mt-[6%] mb-[6%] gap-4 ml-3 mr-3">
         <div
           className="idenity grid grid-cols-2 "
---
+++ src/components/common/CompoHeader.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const CompoHeader = ({name,image}) => {
+
+    const imageStyle = {
+        width: "100%",
+      };
+    
+      const textOnImageStyle = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        fontSize: "70px",
+        // fontStyle: "bold",
+        lineHeight: "0.02rem",
+        marginTop: "0.8rem",
+      };
+    const containerStyle = {
+        position: "relative",
+        textAlign: "center",
+        color: "rgb(201, 236, 236)",
+      };
+
+      const links = {
+        position: "absolute",
+        top: "37%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        fontSize: "23px",
+        fontFamily: "cursive",
+        display: "flex",
+        justifyContent: "center",
+        width: "100%",  
+      };
+    
+      const linkStyle = {
+        margin: "0 15px",
+        transition: "all 0.2s ease-in-out",
+        // marginTop: "9%", // Adjust the margin as needed
+      };
+
+      
+  return (
+    <div style={containerStyle}>
+    <img
+      className="contactimage h-[400px]"
+      src={image}
+      alt="Contact Us"
+      style={imageStyle}
+    />
+    <div className="flex justify-between items-center " style={links}>
+      <a className="hover:text-yellow-400" style={linkStyle} href="/">
+        Home
+      </a>
+      <h1 style={linkStyle}>{name}</h1>
+    </div>
+    <h1 style={textOnImageStyle}>{name}</h1>
+  </div>
+  )
+}
+
+export default CompoHeader
