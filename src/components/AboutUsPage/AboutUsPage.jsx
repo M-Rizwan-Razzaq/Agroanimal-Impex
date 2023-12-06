bfe9d9a597a1fed85a3479adc23deb84357bc710
unknown <shivamswami8899@gmail.com>
2023-12-06 22:53:03 +0530

    85% about us done

--- src/components/AboutUsPage/AboutUsPage.jsx
+++ src/components/AboutUsPage/AboutUsPage.jsx
@@ -4,6 +4,7 @@ import CompoHeader from "../common/CompoHeader";
 import About from "../About/About";
 import secImg from "../../images/TriasseaIcon.png";
 import { Swiper, SwiperSlide } from "swiper/react";
+import bgImage from "../../images/aboutus-back.png";
 import "swiper/css";
 import { Autoplay } from "swiper/modules";
 import { imageUrls } from "./aboutdata";
@@ -47,49 +48,55 @@ const AboutUsPage = () => {
       </section>
 
       <section className="grid grid-cols-1 mb-10 mt-20 md:grid-cols-2 lg:grid-cols-2">
-        <div className="ml-4">
-          <div className="mb-6 md:ml-1 lg:ml-2 ">
-            <span className="flex justify-start items-center gap-1">
-              <img className="w-6" src={secImg} alt="" />
-              <h3 className="text-xl md:text-xl lg:text-xl antialiased font-sans">
-                Why Choose Us
-              </h3>
-            </span>
-            <h1 className="form-hedding text-[30px] md:text-[50px]">
-              We Are Different From Other Farming
-            </h1>
-            <p className="my-1 text-lg text-gray-600">
-              Lorem ipsum dolor sit amet, consectetur adipisic ing elit, sed do
-              eiusmod tempor incididu ut labore et dolore magna aliqu.
-            </p>
-            <a
-              href="/"
-              className="inline-flex items-center justify-center px-5 py-3 text-base md:text-lg lg:text-xl font-medium text-center text-white bg-red-500 rounded-lg hover:bg-green-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ml-4 mt-8"
-            >
-              Read More
-              <svg
-                className="w-5 h-5 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
+        <div>
+          <div className="relative">
+            <img src={bgImage} className="opacity-30" alt="" />
+
+            <div className="absolute top-[10%] left-0 ml-4">
+              <div className="mb-6 md:ml-1 lg:ml-2">
+                <span className="flex justify-start items-center gap-1">
+                  <img className="w-6" src={secImg} alt="" />
+                  <h3 className="text-xl md:text-xl lg:text-xl antialiased font-sans">
+                    Why Choose Us
+                  </h3>
+                </span>
+                <h1 className="form-hedding text-[30px] md:text-[50px]">
+                  We Are Different From Other Farming
+                </h1>
+                <p className="my-1 text-lg text-gray-700">
+                  Lorem ipsum dolor sit amet, consectetur adipisic ing elit, sed
+                  do eiusmod tempor incididu ut labore et dolore magna aliqu.
+                </p>
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 text-base md:text-lg lg:text-xl font-medium text-center text-white bg-red-500 rounded-lg hover:bg-green-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ml-4 mt-8"
+                >
+                  Read More
+                  <svg
+                    className="w-5 h-5 ms-2 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="p-[2%] grid grid-cols-1 md:grid-cols-2 gap-4 mt-8 md:mt-0 lg:ml-10 lg:mt-5 ml-6 text-black">
           <div className="aboutus-grid m-2">
             <div className="aboutus-small flex flex-row mt-5 justify-between gap-40 text-5xl">
-              <GiThreeLeaves />
+              <GiThreeLeaves className="symbol1" />
               <h1 className="">01</h1>
             </div>
             <div>
@@ -101,7 +108,7 @@ const AboutUsPage = () => {
           </div>
           <div className="aboutus-grid m-2">
             <div className="aboutus-small flex flex-row mt-5 justify-between gap-40 text-5xl">
-              <LiaTruckMonsterSolid />
+              <LiaTruckMonsterSolid className="symbol1" />
               <h1 className="">02</h1>
             </div>
             <div>
@@ -113,7 +120,7 @@ const AboutUsPage = () => {
           </div>
           <div className="aboutus-grid m-2">
             <div className="aboutus-small flex flex-row mt-5 justify-between gap-40 text-5xl">
-              <CiDeliveryTruck />
+              <CiDeliveryTruck className="symbol1" />
               <h1 className="">03</h1>
             </div>
             <div>
@@ -125,7 +132,7 @@ const AboutUsPage = () => {
           </div>
           <div className="aboutus-grid m-2">
             <div className="aboutus-small flex flex-row mt-5 justify-between gap-40 text-5xl">
-              <GrDropbox />
+              <GrDropbox className="symbol1" />
               <h1 className="">04</h1>
             </div>
             <div className="mt-4">
